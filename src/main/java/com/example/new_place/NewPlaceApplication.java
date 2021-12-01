16da67bf463306b6eb2a7177c59803db191b43d7
SSCTuatara-Lee <sscleext@163.com>
2021-12-01 22:19:09 +0800

    git 测试提交

---
+++ src/main/java/com/example/new_place/NewPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.new_place;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewPlaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/new_place/NewPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_place;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewPlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
